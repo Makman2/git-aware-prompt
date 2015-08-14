25a14ac77f1c8f11df5cf58af856f6ee375adbe5
Makman2 <mak95@arcor.de>
2015-08-14 03:15:57 +0200

    Implement EXTENDED behaviour
    The extended behaviour resembles more the git shell. It displays
    information about staged and unstaged files.

--- git_aware.sh
+++ git_aware.sh
@@ -1,3 +1,7 @@
+get_git_porcelain_status() {
+  git_porcelain_status=$(git status --porcelain 2> /dev/null)
+}
+
 find_git_branch() {
   # Based on: http://stackoverflow.com/a/13003854/170413
   local branch
@@ -12,12 +16,71 @@ find_git_branch() {
 }
 
 find_git_dirty() {
-  local status=$(git status --porcelain 2> /dev/null)
-  if [[ "$status" != "" ]]; then
+  if [[ "$git_porcelain_status" != "" ]]; then
     git_dirty='*'
   else
     git_dirty=''
   fi
 }
 
-PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
+find_git_file_changes() {
+  if [ -n "$git_branch" ]; then
+    git_staged_added_files=0
+    git_staged_changed_files=0
+    git_staged_removed_files=0
+
+    git_unstaged_added_files=0
+    git_unstaged_changed_files=0
+    git_unstaged_removed_files=0
+
+    git_renamed_files=0
+
+    # Need to manipulate IFS so preceding and leading spaces are not trimmed.
+    local old_IFS="$IFS"
+    IFS=""
+
+    while read line; do
+      if [[ "$line" == ?"M"* ]]; then
+        ((git_unstaged_changed_files++))
+      fi
+
+      if [[ "$line" == "M"* ]]; then
+        ((git_staged_changed_files++))
+      fi
+
+      if [[ "$line" == ?"D"* ]]; then
+        ((git_unstaged_removed_files++))
+      fi
+
+      if [[ "$line" == "D"* ]]; then
+        ((git_staged_removed_files++))
+      fi
+
+      if [[ "$line" == "??"* ]]; then
+        ((git_unstaged_added_files++))
+      fi
+
+      if [[ "$line" == "A"* ]]; then
+        ((git_staged_added_files++))
+      fi
+
+      if [[ "$line" == "R"* ]]; then
+        ((git_renamed_files++))
+      fi
+    done <<< "$git_porcelain_status"
+
+    IFS="$old_IFS"
+  else
+    git_staged_added_files=""
+    git_staged_changed_files=""
+    git_staged_removed_files=""
+
+    git_unstaged_added_files=""
+    git_unstaged_changed_files=""
+    git_unstaged_removed_files=""
+
+    git_renamed_files=""
+  fi
+}
+
+PROMPT_COMMAND="get_git_porcelain_status; find_git_branch; find_git_dirty; find_git_file_changes; $PROMPT_COMMAND"
--- setPS1.sh
+++ setPS1.sh
@@ -6,7 +6,30 @@ update_git_PS1() {
 
   PS1_text=""
   if [ -n "$git_branch" ]; then
-    PS1_text="$PS1_text \\[$txtcyn\\]($git_branch)\\[$txtred\\]$git_dirty\\[$txtrst\\]"
+    PS1_text="$PS1_text \\[$txtcyn\\]($git_branch)"
+
+    if [ "$GITAWAREPROMPT_MODE" == "EXTENDED" ]; then
+      extra_space=""
+
+      ((git_unstaged_files=git_unstaged_added_files+git_unstaged_changed_files+git_unstaged_removed_files))
+      if (( git_unstaged_files > 0 )); then
+        PS1_text="$PS1_text \\[$txtred\\]+$git_unstaged_added_files ~$git_unstaged_changed_files -$git_unstaged_removed_files"
+        extra_space=" "
+      fi
+
+      # Count renamed files to changed files.
+      ((git_staged_files=git_staged_added_files+git_staged_changed_files+git_staged_removed_files+git_renamed_files))
+      if (( git_staged_files > 0 )); then
+        ((git_staged_changed_files+=git_renamed_files))
+        PS1_text="$PS1_text \\[$txtgrn\\]+$git_staged_added_files ~$git_staged_changed_files -$git_staged_removed_files"
+        extra_space=" "
+      fi
+
+      PS1_text="$PS1_text\\[$txtrst\\]$extra_space"
+    else
+      PS1_text="$PS1_text\\[$txtred\\]$git_dirty\\[$txtrst\\]"
+    fi
+
   fi
 
   export PS1="$PS1_prefix$PS1_text$PS1_postfix "
